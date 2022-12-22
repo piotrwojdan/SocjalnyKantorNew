b33d28a338eb084437e5808071fcc386ce45b05c
piotrwojdan <piotr.wojdan2001@gmail.com>
2022-12-22 23:51:45 +0100

    tiding up

--- backend/users/users_application.py
+++ backend/users/users_application.py
@@ -1,8 +1,51 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from abc import ABCMeta
 
 users_app = Flask(__name__)
 
 users_app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 users_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 users_db = SQLAlchemy(users_app)
+
+
+class User(metaclass=ABCMeta):
+    login = ''
+    haslo = ''
+    imie = ''
+    nazwisko = ''
+    dataUrodzenia = datetime.utcnow
+
+
+class Klient(users_db.Model):
+    __tablename__ = "Klient"
+    id = users_db.Column(users_db.Integer, primary_key=True)
+    imie = users_db.Column(users_db.String(20), unique=True, nullable=True)
+    login = users_db.Column(users_db.String(100), unique=True, nullable=False) #login to email
+    # image_file = users_db.Column(users_db.String(20), nullable=True, default='default.jpg')
+    haslo = users_db.Column(users_db.String(60), nullable=False)
+    dataUrodzenia = users_db.Column(users_db.Date, nullable=True)
+    # posts = users_db.relationship('Post', backref='author', lazy=True)    #relacja
+    #rachunki = db.relationship('Rachunek', backref='author', lazy=True)
+
+    # @login_manager.user_loader
+    # def load_user(user_id):
+    #     return Klient.query.get(int(user_id))  # był tu user
+
+    def __repr__(self):
+        return f"Użytkownik {self.imie}, {self.login}, {self.image_file}"
+        
+
+class Administrator(users_db.Model): #
+
+    id = users_db.Column(users_db.Integer, primary_key=True)
+    imie = users_db.Column(users_db.String(20), unique=True, nullable=True)
+    login = users_db.Column(users_db.String(100), unique=True, nullable=False) #login to email
+    # image_file = db.Column(db.String(20), nullable=True, default='default.jpg')
+    haslo = users_db.Column(users_db.String(60), nullable=False)
+    dataUrodzenia = users_db.Column(users_db.Date, nullable=True)
+    # posts = db.relationship('Post', backref='author', lazy=True)    #relacja
+
+    def __repr__(self):
+        return f"Użytkownik {self.imie}, {self.login}, {self.image_file}"
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
+import PostsList from './components/PostsList';
 
 function App() {
 
@@ -19,19 +19,46 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-      <h1>Posty</h1>
-      {posts.map(p => {
-        return (
-          <div key = {p.id}>
-            <h3>{p.tytul}</h3>
-            <p>{p.tresc}</p>
-            <p>{p.dataUtworzenia}</p>
+    <div>
+    <header className="site-header">
+      <nav className="navbar navbar-expand-md navbar-dark bg-steel fixed-top">
+        <div className="container">
+          <a className="navbar-brand mr-4" href="/">Socjalny Kantor</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggle" aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarToggle">
+            <div className="navbar-nav mr-auto">
+              <a className="nav-item nav-link" href="{{ url_for('home') }}">Posty</a>
+                {/* {if current_user.is_authenticated %}
+                    <a class="nav-item nav-link" href="/exchange">Wymiana walut</a>
+                    <a class="nav-item nav-link" href="/plots">Wykresy</a>
+                {% endif %} */}
+              <a className="nav-item nav-link" href="/about">O nas</a>
+            </div>
+            {/* <!-- Navbar Right Side --> */}
+            <div className="navbar-nav">
+              {/* {% if current_user.is_authenticated %}
+                  <a class="nav-item nav-link" href={{ url_for('new_post') }}>Nowy Post</a>
+                  <a class="nav-item nav-link" href="/account">Konto</a>
+                  <a class="nav-item nav-link" href="/logout">Wyloguj</a> */}
+              {/* {% else %} */}
+                  <a className="nav-item nav-link" href="/login">Logowanie</a>
+                  <a className="nav-item nav-link" href="/register">Rejestracja</a>
+              {/* {% endif %} */}
+            </div>
           </div>
-        )
-      }
-      )
-      }
+        </div>
+      </nav>
+    </header>
+    <main className="container">
+      <div className="row">
+        <div className="col-md-8">
+        <h1>Posty</h1>
+        <PostsList posts={posts}></PostsList>
+        </div>
+      </div>
+    </main>
     </div>
   );
 }
