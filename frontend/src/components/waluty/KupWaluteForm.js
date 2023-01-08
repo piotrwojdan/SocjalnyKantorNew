c4ffe4476e88e413a6c5ab4bd97c72d5b1f17590
piotrwojdan <piotr.wojdan2001@gmail.com>
2023-01-08 10:43:17 +0100

    instalacja biblioteki binance

--- frontend/src/components/waluty/KupWaluteForm.js
+++ frontend/src/components/waluty/KupWaluteForm.js
@@ -6,6 +6,9 @@ import React from "react";
 function KupWaluteForm(props) {
 
     const [currencies, setCurrencies] = useState([]);
+    const [currency, setCurrency] = useState("");
+    const [ilosc, setIlosc] = useState(1)
+    const [kurs, setKurs] = useState(0)
 
     useEffect(() => {
         fetch('http://127.0.0.1:5003/get', {
@@ -17,12 +20,20 @@ function KupWaluteForm(props) {
             .then(resp => resp.json())
             .then(resp => setCurrencies(resp))
             .catch(err => console.log(err))
+        setCurrency('BTC')
+    }, [])
+
+    useEffect(() => {
+        fetch("https://api.binance.com/api/v3/ticker/price?symbol="+currency.symbol+"USDT")
+            .then(resp => resp.json())
+            .then(resp => setKurs(resp['price']))
+            .catch(err => console.log(err))
     }, [])
 
 
     //nie uzywamy states tyle ref bo interesuje nas stan tylko po przycisnieciu submit
-    const tytulInputRef = useRef(); // w tym inpucie tworzymy połączenie z tym
-    const imageInputRef = useRef();
+    const walutaInputRef = useRef(); // w tym inpucie tworzymy połączenie z tym
+    const iloscInputRef = useRef();
     const TrescInputRef = useRef();
 
     //react automatycznie przekaż event do tej funkcji jeśli wpiszemy ją w onSubmit
@@ -30,41 +41,56 @@ function KupWaluteForm(props) {
         event.preventDefault() //zapobiegamy wysyłaniu żadania przez przeglądarkę
         //czytamy podane przez użytkownika wartości
 
-        const enteredTytul = tytulInputRef.current.value //obecna wartość
-        const enteredDate = new Date()
-        const enteredTresc = TrescInputRef.current.value
+        const enteredWaluta = walutaInputRef.current.value //obecna wartość
+        // const enteredDate = new Date()
+        const enteredilosc = iloscInputRef.current.value
 
-        const meetupData = {
-            tytul: enteredTytul,
-            data: enteredDate,
-            tresc: enteredTresc
+        const transactionData = {
+            waluta: enteredWaluta,
+            // data: enteredDate,
+            ilosc: enteredilosc
         }
-        console.log(meetupData)
+        console.log(transactionData)
 
-        props.onSubmitForm(meetupData)
+        props.onSubmitForm(transactionData)
     }
 
-    return <Card>
-        <form className={classes.form} onSubmit={submitHandler}>
-            <div className={classes.control}>
-                <label htmlFor={'waluta'}>Waluta</label>
-                <select type="text" required id="tytul" ref={tytulInputRef}>
-                    {currencies.map((cur) => { return <option value={cur.symbol}>{cur.symbol}</option>})}
-                </select>
-            </div>
-            <div className={classes.control}>
-                <label htmlFor={'Tresc'}>Treść</label>
-                <textarea
-                    id="Tresc"
-                    required rows="5"
-                    ref={TrescInputRef}
-                ></textarea>
-            </div>
-            <div className={classes.action}>
-                <button>Dodaj post</button>
-            </div>
-        </form>
-    </Card>
+    return (
+        <Card>
+            <form className={classes.form} onSubmit={submitHandler}>
+                <div className={classes.control}>
+                    <label htmlFor={'waluta'}>Waluta</label>
+                    <select type="text" required id="waluta" ref={walutaInputRef}>
+                        {currencies.map((cur) => { return <option value={cur.symbol}>{cur.symbol}</option> })}
+                    </select>
+                </div>
+                <div className={classes.control}>
+                    <label htmlFor={'ilosc'}>Ilość</label>
+                    <input
+                        type="number"
+                        id="ilosc"
+                        min="0"
+                        defaultValue={ilosc}
+                        onChange={(event) => {
+                            setIlosc(event.target.value)
+                            return (event.charCode !=8 && event.charCode ==0 || (event.charCode >= 48 && event.charCode <= 57))
+                        }}
+                        ref={iloscInputRef} />
+                </div>
+                <div className={classes.control}>
+                    <label htmlFor={'kurs'}>Kurs</label>
+                </div>
+                <label id="kurs">{kurs}</label>
+                <div className={classes.control}>
+                    <label htmlFor={'cena'}>Cena</label>
+                </div>
+                <label id="cena">{ilosc * kurs}</label>
+                <div className={classes.actions}>
+                    <button>Kup</button>
+                </div>
+            </form>
+        </Card>
+    )
 }
 
 export default KupWaluteForm;
